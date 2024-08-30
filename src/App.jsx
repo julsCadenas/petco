4af3a14ff23aa221329c602a7c972870be1bf43d
julsCadenas <jscadenas12@gmail.com>
2024-08-30 11:49:31 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-500'>Urmom</div>
+    </>
+  )
+}
+
+export default App
