f2174036636ca1b331ba016abddc5195defec76f
julsCadenas <jscadenas12@gmail.com>
2024-08-31 00:27:19 +0800

    changes to stats component

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -14,8 +14,8 @@ const Stats = () => {
 
 
   return (
-    <main className='font-poppins h-full py-10 pb-28'>
-        <section className='flex md:flex-row pb-6 text-3xl md:text-6xl font-extrabold text-red2 items-center justify-center'>
+    <main className='font-poppins h-full py-10 pb-28 flex items-center flex-col'>
+        <section className='flex md:flex-row pb-8 text-4xl md:text-6xl font-extrabold text-red2 items-center justify-center'>
             <p className='pr-3'>Why</p>
         {text.split("").map((char, index) => (
           <span 
@@ -29,7 +29,8 @@ const Stats = () => {
         ))}
             <p>?</p>
         </section>
-        <section className='flex flex-wrap gap-10 justify-center pb-2'>
+        <hr className='border-b-blue2 border-b-4 w-1/2 md:w-1/3 mb-10' />
+        <section className='flex flex-wrap gap-10 justify-center'>
             {statistics.map((stat, index) => (
                 <aside key={index} className='w-60 p-5 border-4 border-red2 rounded-xl flex flex-col items-center font-semibold text-center'>
                     <p className='border-b-red2 pb-3 border-b-4 text-5xl font-bold'>{stat.number}</p>
