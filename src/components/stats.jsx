bd013495c3dd282a6da09db2ff9cf86509d3f636
julsCadenas <jscadenas12@gmail.com>
2024-08-31 00:16:27 +0800

    stats component: added more spacing between boxes and increased title font size

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -15,7 +15,7 @@ const Stats = () => {
 
   return (
     <main className='font-poppins h-full py-10 pb-28'>
-        <section className='flex md:flex-row pb-5 text-3xl md:text-5xl font-extrabold text-red2 items-center justify-center'>
+        <section className='flex md:flex-row pb-6 text-3xl md:text-6xl font-extrabold text-red2 items-center justify-center'>
             <p className='pr-3'>Why</p>
         {text.split("").map((char, index) => (
           <span 
@@ -29,7 +29,7 @@ const Stats = () => {
         ))}
             <p>?</p>
         </section>
-        <section className='flex flex-wrap gap-2 justify-center pb-2'>
+        <section className='flex flex-wrap gap-10 justify-center pb-2'>
             {statistics.map((stat, index) => (
                 <aside key={index} className='w-60 p-5 border-4 border-red2 rounded-xl flex flex-col items-center font-semibold text-center'>
                     <p className='border-b-red2 pb-3 border-b-4 text-5xl font-bold'>{stat.number}</p>
