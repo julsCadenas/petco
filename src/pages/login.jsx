14c908898dc9381154f657569dc464a4608003b7
julsCadenas <jscadenas12@gmail.com>
2024-08-31 11:07:00 +0800

    semi finished sign up page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import signuppic from '../assets/signuppic.png';
+import { Link } from 'react-router-dom';  
 
 const Login = () => {
   const text = "Noot Note";
@@ -9,24 +10,35 @@ const Login = () => {
   return (
     <main className='font-poppins flex flex-wrap gap-5 items-center justify-center h-screen '>
       
-      <figure>
+      <figure className='hidden md:flex'>
         <img src={signuppic} alt="sign up" className='rounded-xl w-[400px] ' />
       </figure>
 
       <div className='flex flex-col'>
 
-        <aside className='flex justify-end items-center gap-3 pb-10 text-sm'>
-          <p>Already have an account?</p>
-            <span>
-              <button className='border border-blue2 rounded-full px-3 py-1'>Sign in</button>
+      <section className='flex justify-between items-center pb-10'>
+        <aside>
+          <Link to='/landing' className='bg-blue2 rounded-full text-white2 flex items-center'>
+            <span className="material-symbols-outlined">
+              chevron_left
             </span>
-          </aside>
+          </Link>
+        </aside>
+
+        <aside className='flex items-center gap-3 text-xs md:text-sm'>
+          <p>Already have an account?</p>
+          <span>
+            <button className='border border-blue2 hover:bg-blue2 hover:text-white2 transition-colors rounded-full px-3 py-1'>Sign in</button>
+          </span>
+        </aside>
+      </section>
+
 
         <section className=''>
 
           <section className=''>
 
-            <div className='flex font-bold text-4xl'>
+            <div className='flex font-bold text-2xl md:text-4xl'>
               <p className='pr-2'>Welcome to</p>
               {text.split("").map((char, index) => (
                 <span 
@@ -52,22 +64,22 @@ const Login = () => {
           <section>
             <div>
               <p>Name</p>
-              <input type="text" placeholder='Full Name' className='mb-3 text-gray-400 text-sm pl-3 focus:outline-none focus:text-blue2 focus:border-blue2 hover:border-blue2 hover:text-blue2  transition-all border border-black rounded-md w-[400px] h-9' />
+              <input required type="text" placeholder='Full Name' className='mb-3 text-gray-400 text-sm pl-3 focus:outline-none focus:text-blue2 focus:border-blue2 hover:border-blue2 hover:text-blue2  transition-all border border-black rounded-md w-[400px] h-9' />
             </div>
             <div>
               <p>Email</p>
-              <input type="email" placeholder='example@email.com' className='mb-3 text-gray-400 text-sm pl-3 focus:outline-none focus:text-blue2 focus:border-blue2 hover:border-blue2 hover:text-blue2  transition-all border border-black rounded-md w-[400px] h-9' />
+              <input required type="email" placeholder='example@email.com' className='mb-3 text-gray-400 text-sm pl-3 focus:outline-none focus:text-blue2 focus:border-blue2 hover:border-blue2 hover:text-blue2  transition-all border border-black rounded-md w-[400px] h-9' />
             </div>
             <div>
               <p>Name</p>
-              <input type="password" placeholder='8+ characters' className='mb-3 text-gray-400 text-sm pl-3 focus:outline-none focus:text-blue2 focus:border-blue2 hover:border-blue2 hover:text-blue2  transition-all border border-black rounded-md w-[400px] h-9' />
+              <input required type="password" placeholder='8+ characters' className='mb-3 text-gray-400 text-sm pl-3 focus:outline-none focus:text-blue2 focus:border-blue2 hover:border-blue2 hover:text-blue2  transition-all border border-black rounded-md w-[400px] h-9' />
             </div>
           </section>
           <div className='flex justify-center -ml-5'>
             <button className='bg-blue2 w-36 h-10 rounded-full text-white2' >Sign Up</button>
           </div>
           <div className='text-xs flex gap-2'>
-            <input type="checkbox" className='border-blue2'/>
+            <input type="checkbox" className='border-blue2' required/>
             <p>I have read and accept the <strong className='text-yellow2'>Terms of Service</strong> & <strong className='text-red2'>Privacy Policy</strong></p>
           </div>
         </section>
